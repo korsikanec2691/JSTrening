0b58a31b5f67b19c30a74540dd8c6e1b95f14bc4
Rustamov Rovwan <rus2691@yandex.ru>
2021-12-21 21:32:47 +0300

    Nev

--- Алекс Лущенко/JS/Домашка JS/app12/js/script11.js
+++ Алекс Лущенко/JS/Домашка JS/app12/js/script11.js
@@ -6,28 +6,38 @@ let b = [
     [4,5,6],
     [7,8,9],
 ]
-console.log(b);
+// console.log(b);
 // for (let i = 0; i < b.length; i++) {
 
 //     // console.log(b[i]);
 //     let c = b[i];
-//     for (let k = c.length; k >= 0; k--) {
+//     for (let k = c.length - 1; k >= 0; k--) {
 
 //         console.log(c[k]);
 //     }
-
-
 // }
 
+ let out = '';
+
+// for (let i = 0; i < b.length; i++) {
+//     for (let k = 0; k < b[i].length; k++) {
+//         // console.log(b[i][k])
+//         out += b[i][k];
+//     }
+//     out +=' <br>'
+// }
+
+document.querySelector('.out').innerHTML = out;
+
+ out += '';
 for (let i = 0; i < b.length; i++) {
     for (let k = 0; k < b[i].length; k++) {
-        console.log(b[i][k])
+        // console.log(b[i][k])
+        if(b[i][k] > 4) {
+        out += b[i][k] + ' ';
+        }
     }
+    out +='<br>'
 }
-
-
-
-
-
-
+console.log(out);
 
